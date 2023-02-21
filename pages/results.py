29ea05114f5878abe4c472b290f40a27b958bc07
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-21 10:30:55 -0700

    hola

--- pages/results.py
+++ pages/results.py
@@ -63,7 +63,7 @@ for element in st.session_state['result1']:
     fullDescription = element[4]
     with st.expander(f"{title} at {companyName}", expanded= False):
         st.write(f"[Apply]({link})")
-        for item in fullDescription.split('.')[:3]:
+        for item in fullDescription.split('.')[:5]:
             st.write(item)
         # st.write(fullDescription.split('.')[:3])
         if st.button("Generate Cover Letter", key=f"{link}+{title}"):
