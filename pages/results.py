ebdc75d1f67e3d06cde29231a80066955040c768
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-22 16:50:12 -0700

    hola

--- pages/results.py
+++ pages/results.py
@@ -71,9 +71,9 @@ for element in st.session_state['result1']:
     fullDescription = element[4]
     with st.expander(f"{title} at {companyName}", expanded= False):
         st.write(f"[Apply]({link})")
-        for item in fullDescription.split('.')[:5]:
-            st.write(item)
-        # st.write(fullDescription.split('.')[:3])
+        # for item in fullDescription.split('.')[:5]:
+        #     st.write(item)
+        st.write(shortSummary)
         if st.button("Generate Cover Letter", key=f"{link}+{title}"):
                 responseJob = openai.Completion.create(
                     model="text-davinci-003",
