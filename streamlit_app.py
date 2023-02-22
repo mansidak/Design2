ef8c3b1ff13441c32593097c7c2aaab3f499a9c1
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-21 21:45:25 -0700

    hola

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 from streamlit_extras.switch_page_button import switch_page
 
 
-st.set_page_config(page_title="9th Street", layout="wide", page_icon='✨', initial_sidebar_state='collapsed')
+st.set_page_config(page_title="19th Street", layout="wide", page_icon='⓵⓽', initial_sidebar_state='collapsed')
 
 st.markdown(
     """
