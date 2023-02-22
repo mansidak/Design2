00c3667a0877b55d583d0af9343ad022a3cf37aa
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-21 18:24:44 -0700

    hola

--- pages/results.py
+++ pages/results.py
@@ -33,20 +33,10 @@ hide_streamlit_style = """
               """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 footer="""<style>
-a:link , a:visited{
-color: blue;
-background-color: transparent;
-text-decoration: underline;
-}
 
-a:hover,  a:active {
-color: red;
-background-color: transparent;
-text-decoration: underline;
-}
 
 .footer {
-position: fixed;
+position: absolute;
 left: 0;
 bottom: 0;
 width: 100%;
