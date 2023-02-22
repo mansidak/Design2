680332d7f028fa750f0096a7ef218dcff18097f9
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-21 18:24:19 -0700

    hola

--- pages/results.py
+++ pages/results.py
@@ -32,7 +32,34 @@ hide_streamlit_style = """
               </style>
               """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
 
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: 0d0d0d;
+color: 2A2A2A;
+text-align: center;
+}
+</style>
+<div class="footer" font-family: Sans-Serif;font-weight: lighter;>
+<p>A Mansidak Singh Production</p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
 col1, col2, col3 = st.columns([2, 1, 2])
 
 with col1:
