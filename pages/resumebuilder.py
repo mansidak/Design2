45490c2ef55d1efc25d01e7c1e480fc2cbd33c1d
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-23 15:29:15 -0700

    hola

--- pages/resumebuilder.py
+++ pages/resumebuilder.py
@@ -96,6 +96,11 @@ with col2:
 
     with tab1:
         st.header("Details")
+        st.text_area(
+            'Caption goes here',
+            placeholder='Placeholder goes here - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to ',
+            help='Help message goes here'
+        )
 
     with tab2:
         st.header("Experiences")
