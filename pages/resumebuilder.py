c633bbb316807b18e41797a1ebd625451744bbe9
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-23 15:31:32 -0700

    hola

--- pages/resumebuilder.py
+++ pages/resumebuilder.py
@@ -81,7 +81,7 @@ with col1:
     st.write("")
 
 with col2:
-    tab1, tab2, tab3, tab4 = st.tabs(["\u2001Basics\u2001", "   Experience   ", "   Projects   ","   Interests   "])
+    tab1, tab2, tab3, tab4 = st.tabs(["\u2001Basics\u2001", "\u2001\u2001Experience\u2001\u2001", "\u2001\u2001Projects\u2001\u2001","\u2001\u2001Interests\u2001\u2001"])
 
     with tab1:
         st.header("Details")
