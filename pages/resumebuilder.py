620fea5a95e6fb02ada59d3e567148317b35cb65
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-23 09:49:35 -0700

    hola

--- pages/resumebuilder.py
+++ pages/resumebuilder.py
@@ -81,8 +81,19 @@ button[title="View fullscreen"]{
 
 st.markdown(hide_img_fs, unsafe_allow_html=True)
 
-st.text_area(
-  'Enter Experience',
-  placeholder="Write briefly about your experience in less than a paragraph. We'll take care of the rest. ",
-  help='Help message goes here'
-)
+
+col1, col2, col3 = st.columns([2, 1, 2])
+
+with col1:
+    st.text_area(
+        'Enter Experience',
+        placeholder="Write briefly about your experience in less than a paragraph. We'll take care of the rest. ",
+        help='Help message goes here'
+    )
+
+with col2:
+    st.write("")
+
+with col3:
+    st.write("")
+
