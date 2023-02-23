51ddd7412d562ceaffe49cef0c0ef4a3fa531761
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-23 10:11:59 -0700

    hola

--- pages/resumebuilder.py
+++ pages/resumebuilder.py
@@ -82,7 +82,7 @@ button[title="View fullscreen"]{
 st.markdown(hide_img_fs, unsafe_allow_html=True)
 
 
-col1, col2, col3, col4 = st.columns([2, 1, 1, 2])
+col1, col2, col3, col4 = st.columns([1, 2, 2, 1])
 
 with col1:
     st.write("")
