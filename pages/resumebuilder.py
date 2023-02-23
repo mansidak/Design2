f7d1e0d7ba5712abca85974755833b028b59d02d
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-23 09:47:09 -0700

    hola

--- pages/resumebuilder.py
+++ pages/resumebuilder.py
@@ -6,24 +6,24 @@ from docx import Document
 from PIL import Image
 from streamlit_extras.switch_page_button import switch_page
 
-
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-color: #eeeeee
-    }
-
-    .css-hckj40{
-        background-color: #eeeeee
-    }
-   .sidebar .sidebar-content {
-        background: #eeeeee
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+#
+# st.markdown(
+#     """
+#     <style>
+#     .stApp {
+#         background-color: #eeeeee
+#     }
+#
+#     .css-hckj40{
+#         background-color: #eeeeee
+#     }
+#    .sidebar .sidebar-content {
+#         background: #eeeeee
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 hide_menu_style = """
          <style>
          #MainMenu {visibility: hidden;}
