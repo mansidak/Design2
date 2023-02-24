1da72039cc321609be786c6bab7dbc084c1fcb57
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-24 11:10:09 -0700

    hola

--- pages/PreResumeBuilder.py
+++ pages/PreResumeBuilder.py
@@ -63,7 +63,7 @@ with col2:
     ResumePDF = st.file_uploader(
         ''
     )
-    col1, col2, col3 = st.columne([1,2,1])
+    col1, col2, col3 = st.columns([1,2,1])
     with col1:
        st.write("")
     with col2:
