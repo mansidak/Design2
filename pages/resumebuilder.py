d66983d7d3301185f5819ec4f34f02a02ace94d5
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-23 19:01:14 -0700

    hola

--- pages/resumebuilder.py
+++ pages/resumebuilder.py
@@ -305,14 +305,15 @@ with tab4:
     if st.button("Add Skills"):
         response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f"The following is some experience of a job seeker.\n\n{Experience1Name}\n{Experience1Description}\n\n{Experience2Name}\n{Experience2Description}\n\n{Experience3Name}\n{Experience3Description}\n\n{Experience4Name}\n{Experience4Description} \n What kind of technical skills they have? List them in bullet points.\n",
+        prompt=f"The following is some experience of a job seeker.\n\n{Experience1Name}\n{Experience1Description}\n\n{Experience2Name}\n{Experience2Description}\n\n{Experience3Name}\n{Experience3Description}\n\n{Experience4Name}\n{Experience4Description} \n What kind of technical skills they have? List them as spearated by commas.\n",
         temperature=0.7,
-        max_tokens=110,
+        max_tokens=80,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
         )
 
-        st.write(response["choices"][0]["text"])
+
+        options = st.multiselect('Filter through location', response["choices"][0]["text"],response["choices"][0]["text"])
 with tab5:
     st.write("")
