1f40fc61d14baeead6db24f111abecff7a1f60de
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-24 10:43:52 -0700

    hola

--- pages/PreResumeBuilder.py
+++ pages/PreResumeBuilder.py
@@ -3,7 +3,46 @@ import os
 st.set_page_config(page_title="19th Street | Resume Builder", page_icon="⓵⓽", layout = "wide")
 
 
-col1, col2, col3, col4 = st.columns([2, 1, 1, 2])
+hide_menu_style = """
+         <style>
+         #MainMenu {visibility: hidden;}
+         .css-j7qwjs {visibility: hidden;}
+         footer {visibility: hidden;}
+         </style>
+         """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+hide_streamlit_style = """
+              <style>
+              div[class='css-4z1n4l ehezqtx5']{
+                background: rgba(0, 0, 0, 0.3);
+                color: #fff;
+                border-radius: 10px;
+                backdrop-filter: blur(10px);
+                height: 40px;
+                max-width: 200px;
+                position: fixed;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                width: 50%;
+              }
+              </style>
+              """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+
+
+
+
+col1, col2, col3, col4 = st.columns([1, 2, 2, 1])
 
 with col1:
     st.write("")
