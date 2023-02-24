be20172d0d28271f0b0e9e42efa3472872ce09b3
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-23 19:25:27 -0700

    hola

--- pages/resumebuilder.py
+++ pages/resumebuilder.py
@@ -313,7 +313,8 @@ with tab4:
                 frequency_penalty=0,
                 presence_penalty=0
             )
-            st.session_state['Skills'] = response["choices"][0]["text"].split(',')
+            PreSkills = response["choices"][0]["text"].split(',')
+            st.session_state['Skills'] = map(str.strip, PreSkills)
 
             st.text_input(
             'Caption goes here',
