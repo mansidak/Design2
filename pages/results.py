10bc8eb412b16a8f625c95837c28d2c3b8b84a9d
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-25 00:57:49 -0700

    Damn

--- pages/results.py
+++ pages/results.py
@@ -84,8 +84,7 @@ st.write("")
 
 
 st.write(type(st.session_state['FinalResults']))
-for element in shuffle(st.session_state['FinalResults']):
-
+st.write(shuffle(list(st.session_state['FinalResults'])))
 for element in st.session_state['FinalResults']:
     if element[5] in options and element[6] in options2:
         link = element[0]
