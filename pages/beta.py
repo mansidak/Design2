0cc7548521027a4d4e9f34318781e2c4c434bbb6
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-25 09:25:28 -0700

    Damn

--- pages/beta.py
+++ pages/beta.py
@@ -352,11 +352,12 @@ text-align: center;
             if 'resumeContent' not in st.session_state:
                 st.session_state["resumeContent"] = resumeContent
             holder2.empty()
+            DisplaySkills = ', '.join([item.replace('-', '') for item in newSkills])
             NameHolder.markdown(f"<h2 style='text-align: center; font-family: Sans-Serif;'>Welcome,{Name}</h2>", unsafe_allow_html=True)
             progressText.markdown(f"<h6 style='text-align: center; font-family: Sans-Serif;font-weight: lighter;'>Looking for jobs where you can use your experience in {newSkills}etc...</h6>",unsafe_allow_html=True)
             my_bar.progress(25, text=f"")
             result1 = run_selenium1(f"{newJobtitles[0]}-{ExperienceLevel}", f"{newSkills[0]}", f"{undesired}", 1, resumeContent)
-            progressText.markdown(f"<h6 style='text-align: center; font-family: Sans-Serif;font-weight: lighter;'>You have some background in {softSkills.replace('-', '')}. We're looking for more jobs that match that...</h6>",unsafe_allow_html=True)
+            progressText.markdown(f"<h6 style='text-align: center; font-family: Sans-Serif;font-weight: lighter;'>You have some background in {softSkills}. We're looking for more jobs that match that...</h6>",unsafe_allow_html=True)
             my_bar.progress(50, text=f"")
             result2 = run_selenium1(f"{newJobtitles[1]}-{ExperienceLevel}", f"{newSkills[1]}", f"{undesired}", 1, resumeContent)
             progressText.markdown(f"<h6 style='text-align: center; font-family: Sans-Serif;font-weight: lighter;'>Hold tight!Doing one last search....</h6>",unsafe_allow_html=True)
