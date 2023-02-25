85388f262c216152a2783bdfe2b4c3e8c064d94b
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-24 22:26:43 -0700

    Damn

--- pages/results.py
+++ pages/results.py
@@ -291,7 +291,7 @@ for element in st.session_state['FinalResults']:
                 button_A = container_2.button('Generate Cover Letter', key=f"{link}+{title}+{shortSummary}")
                 if button_A:
                     container_2.empty()
-                    button_B = container_2.button('Generating... Please wait.', key=f"{link}+{title}+{shortSummary}")
+                    button_B = container_2.button('Generating... Please wait.', key=f"{link}+{title}+{shortSummary}+Generating")
                     responseJob = openai.Completion.create(
                         model="text-davinci-003",
                         prompt=f"summarize the job: {fullDescription}",
