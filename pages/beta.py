99c57cab03c689dbbd3a9133d42bcb5be74ddab5
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-25 00:34:02 -0700

    Damn

--- pages/beta.py
+++ pages/beta.py
@@ -157,7 +157,7 @@ text-align: center;
     with Pool(2) as p:
 
 
-        @st.cache(suppress_st_warning= True)
+        @st.cache_data(suppress_st_warning=True)
         def run_selenium1(jobTitle, skill1, undesired, pageNumber, resumeContent):
             name = str()
             Final_Array = []
@@ -174,7 +174,7 @@ text-align: center;
             options.add_argument('--ignore-certificate-errors')
 
             # lock = threading.Lock()
-            @st.cache(suppress_st_warning=True)
+            @st.cache_data(suppress_st_warning=True)
             def get_links(i, resumeContent):
                 Final_Links = []
                 Final_Titles = []
@@ -264,7 +264,7 @@ text-align: center;
             driver.quit()
             return Final_Array
 
-        @st.cache(suppress_st_warning=True)
+        @st.cache_data(suppress_st_warning=True)
         def openAIGetRelevantJobTitles(resumeContent):
             response = openai.Completion.create(
                 model="text-davinci-003",
@@ -284,8 +284,6 @@ text-align: center;
             softSkills = Titles.split('4.')[1]
             newJobtitles = [item.replace(" ", "-") for item in Jobtitles]
             newSkills = [item.replace(" ", "-") for item in Skills]
-            st.write(newJobtitles)
-            st.write(newSkills)
             return Name, newJobtitles, newSkills
 
         col1, col2, col3 = st.columns([2, 1, 2])
@@ -327,7 +325,7 @@ text-align: center;
                 help="As we develop this program more, we'll add more filter"
             )
 
-        @st.cache(suppress_st_warning=True)
+        @st.cache_data(suppress_st_warning=True)
         def extract_text_from_pdf(pdf_file):
             pdfReader = PyPDF2.PdfReader(pdf_file)
             txtFile = open('sample.txt', 'w')
