a8983818d90b623338a7094adbff2c027fd5d0ed
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-25 00:19:38 -0700

    Damn

--- pages/results.py
+++ pages/results.py
@@ -5,6 +5,7 @@ import openai
 from docx import Document
 from PIL import Image
 from streamlit_extras.switch_page_button import switch_page
+import uuid
 
 hide_menu_style = """
          <style>
@@ -226,7 +227,7 @@ for element in st.session_state['FinalResults']:
 
             with col1:
                 container_2 = st.empty()
-                button_A = container_2.button('Generate Cover Letter', key=f"{link}+{title}+{shortSummary}")
+                button_A = container_2.button('Generate Cover Letter', key=f"{link}+{title}+{shortSummary} +{uuid}")
                 if button_A:
                     container_2.empty()
                     button_B = container_2.button('Generating... Please wait.', key=f"{link}+{title}+{shortSummary}+Generating", disabled=True)
