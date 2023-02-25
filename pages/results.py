2ccff9a7e08acaecc2699bef6fb95c07b91be4fa
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-24 22:09:36 -0700

    Damn

--- pages/results.py
+++ pages/results.py
@@ -72,7 +72,7 @@ st.write("")
 st.write("")
 with st.sidebar:
     options = st.multiselect('Filter through location', set([item[5] for item in st.session_state['FinalResults']]), None)
-    options2 = st.multiselect('Filter through location', set([item[6] for item in st.session_state['FinalResults']]), None)
+    options2 = st.multiselect('Skills you want to use', set([item[6] for item in st.session_state['FinalResults']]), None)
 
 
 for element in st.session_state['FinalResults']:
