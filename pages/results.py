e3da738b05c7aebf07821c61faf4514887e9d301
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-27 10:08:11 -0700

    the one before this.

--- pages/results.py
+++ pages/results.py
@@ -376,6 +376,5 @@ if st.button("Download Items", key="Download PDF"):
         pdf.drawString(100, 650, companyName)
         pdf.drawString(100, 650, companyName)
         pdf.save()
-        st.download_button(label="Download PDF", data=pdf, file_name="my_pdf.pdf", mime="application/pdf", key= "hanji download")
-
+        st.download_button(label="Download PDF", data=pdf.getpdfdata(), file_name="my_pdf.pdf", mime="application/pdf", key= "hanji download")
 
