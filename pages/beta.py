6e8964e51fe6ca33d97ac30563adb6c983666bb9
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-27 08:55:06 -0700

    the one before this.

--- pages/beta.py
+++ pages/beta.py
@@ -379,24 +379,24 @@ text-align: center;
         progressText.markdown(f"<h6 style='text-align: center; font-family: Sans-Serif;font-weight: lighter;'>Looking for jobs where you can use your experience in {DisplaySkills}etc...</h6>", unsafe_allow_html=True)
         my_bar.progress(25, text=f"")
 
-        links1 = run_selenium1(f"{newJobtitles[0]}-{ExperienceLevel}", f"{newSkills[0]}", f"{undesired}", 1, resumeContent)
-        links2 = run_selenium1(f"{newJobtitles[1]}-{ExperienceLevel}", f"{newSkills[1]}", f"{undesired}", 1, resumeContent)
-        links3 = run_selenium1(f"{newJobtitles[0]}-{ExperienceLevel}", f"{newSkills[2]}", f"{undesired}", 1, resumeContent)
+        # links1 = run_selenium1(f"{newJobtitles[0]}-{ExperienceLevel}", f"{newSkills[0]}", f"{undesired}", 1, resumeContent)
+        # links2 = run_selenium1(f"{newJobtitles[1]}-{ExperienceLevel}", f"{newSkills[1]}", f"{undesired}", 1, resumeContent)
+        # links3 = run_selenium1(f"{newJobtitles[0]}-{ExperienceLevel}", f"{newSkills[2]}", f"{undesired}", 1, resumeContent)
 
-        # with ThreadPoolExecutor(max_workers=3) as executor:
-        #     future1 = executor.submit(run_selenium1, f"{newJobtitles[0]}-{ExperienceLevel}", f"{newSkills[0]}", f"{undesired}", 1, resumeContent)
-        #     future2 = executor.submit(run_selenium1, f"{newJobtitles[1]}-{ExperienceLevel}", f"{newSkills[1]}", f"{undesired}", 1, resumeContent)
-        #     future3 = executor.submit(run_selenium1, f"{newJobtitles[0]}-{ExperienceLevel}", f"{newSkills[2]}", f"{undesired}", 1, resumeContent)
-        # executor.shutdown(wait=True)
-        #
-        # # Get the results
-        # links1 = future1.result()
-        # links2 = future2.result()
-        # links3 = future3.result()
-        # executor.shutdown(wait=True)
-        # st.write(links1)
-        # st.write(links2)
-        # st.write(links3)
+        with ThreadPoolExecutor(max_workers=3) as executor:
+            future1 = executor.submit(run_selenium1, f"{newJobtitles[0]}-{ExperienceLevel}", f"{newSkills[0]}", f"{undesired}", 1, resumeContent)
+            future2 = executor.submit(run_selenium1, f"{newJobtitles[1]}-{ExperienceLevel}", f"{newSkills[1]}", f"{undesired}", 1, resumeContent)
+            future3 = executor.submit(run_selenium1, f"{newJobtitles[0]}-{ExperienceLevel}", f"{newSkills[2]}", f"{undesired}", 1, resumeContent)
+        executor.shutdown(wait=True)
+
+        # Get the results
+        links1 = future1.result()
+        links2 = future2.result()
+        links3 = future3.result()
+        executor.shutdown(wait=True)
+        st.write(links1)
+        st.write(links2)
+        st.write(links3)
 
         with ThreadPoolExecutor() as executor:
             futures = [executor.submit(get_links, link, newSkills[0], resumeContent) for link in links1]
