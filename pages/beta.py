259ea0ff77e2b1564c6bb5fa8f88faacbb9694ef
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-27 09:20:28 -0700

    the one before this.

--- pages/beta.py
+++ pages/beta.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
                     backdrop-filter: blur(10px);
                     border-radius: 10px;
                     height: 40px;
-                    max-width: 200px;
+                    max-width: 250px;
                     position: fixed;
                     top: 50%;
                     left: 50%;
