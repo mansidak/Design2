e67fba93fc1dfa1278948db42f1c6a4dceafd14f
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-27 09:02:51 -0700

    the one before this.

--- pages/beta.py
+++ pages/beta.py
@@ -287,10 +287,10 @@ text-align: center;
 
 
     @st.cache(show_spinner=False)
-    def openAIGetRelevantJobTitles(resumeContent):
+    def openAIGetRelevantJobTitles(undesired, resumeContent):
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=f"The following is the data from the resume of a job seeker. I want you to do four things:\n\n1. In addition to what they've already done, what jobs roles could they apply for? List 3 and separate them by commas.\n\n2. List only the top 3 of their strongest skills that they have extensive experience in as seen in their resume. Separate them by commas. \n\n 3. Their Full Name \n\n 4.Their top 3 soft skills  \n\n {resumeContent} \n",
+            prompt=f"The following is the data from the resume of a job seeker. I want you to do four things:\n\n1. In addition to what they've already done, what jobs roles could they apply for except {undesired}? List 3 and separate them by commas.\n\n2. List only the top 3 of their strongest skills that they have extensive experience in as seen in their resume. Separate them by commas. \n\n 3. Their Full Name \n\n 4.Their top 3 soft skills  \n\n {resumeContent} \n",
             temperature=0.7,
             max_tokens=146,
             top_p=1,
@@ -368,7 +368,7 @@ text-align: center;
         holder.empty()
         holder2.empty()
         resumeContent = extract_text_from_pdf(ResumePDF)
-        Name, newJobtitles, newSkills, softSkills = openAIGetRelevantJobTitles(resumeContent)
+        Name, newJobtitles, newSkills, softSkills = openAIGetRelevantJobTitles(undesired, resumeContent)
         if 'Name' not in st.session_state:
             st.session_state['Name'] = Name
         if 'resumeContent' not in st.session_state:
