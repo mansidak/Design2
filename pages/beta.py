368536a151da22e3919ff7949f76bd856b16323d
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-26 17:00:27 -0700

    the one before this.

--- pages/beta.py
+++ pages/beta.py
@@ -51,6 +51,8 @@ def show_selenium_log():
 openai.api_key = os.environ.get("openai_api_key")
 
 if __name__ == "__main__":
+    print(threading.enumerate())
+    st.write(threading.enumerate())
     st.set_page_config(page_title="19th Street", page_icon='⓵⓽',
                        initial_sidebar_state='collapsed')
     hide_streamlit_style = """
