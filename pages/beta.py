6981cd9700f712f466f4cb9c708e51a601331a42
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-26 18:08:37 -0700

    the one before this.

--- pages/beta.py
+++ pages/beta.py
@@ -383,9 +383,8 @@ if __name__ == "__main__":
         links1 = run_selenium1(f"{newJobtitles[0]}-{ExperienceLevel}", f"{newSkills[0]}", f"{undesired}", 1, resumeContent)
         threads =[]
         with ThreadPoolExecutor() as executor:
-            for i in links1:
-                future = executor.map(get_links, i, newSkills[0], resumeContent )
-                result1 = future.result()
+            future1 = executor.map(get_links, *links1, *newSkills[0], resumeContent)
+            result1= list(future1)
 
         # for i in links1:
         #     t = threading.Thread(target=get_links, args=(i,newSkills[0], resumeContent))
@@ -403,9 +402,8 @@ if __name__ == "__main__":
 
         links2 = run_selenium1(f"{newJobtitles[1]}-{ExperienceLevel}", f"{newSkills[1]}", f"{undesired}", 1, resumeContent)
         with ThreadPoolExecutor() as executor:
-            for i in links2:
-                future = executor.map(get_links, i, newSkills[1], resumeContent)
-                result2 = future.result()
+            future2 = executor.map(get_links, *links2, *newSkills[1], resumeContent)
+            result2 = list(future2)
 
         # for i in links2:
         #     t = threading.Thread(target=get_links, args=(i, newSkills[1], resumeContent))
@@ -423,9 +421,8 @@ if __name__ == "__main__":
 
         links3 = run_selenium1(f"{newJobtitles[0]}-{ExperienceLevel}", f"{newSkills[2]}", f"{undesired}", 1, resumeContent)
         with ThreadPoolExecutor() as executor:
-            for i in links3:
-                future = executor.map(get_links, i, newSkills[2], resumeContent)
-                result3 = future.result()
+            future3 = executor.map(get_links, *links3, *newSkills[2], resumeContent)
+            result3 = list(future3)
 
         # for i in links3:
         #     t = threading.Thread(target=get_links, args=(i, newSkills[2], resumeContent))
