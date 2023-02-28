9117d433dbeb0414ca46add2a1e3ce8655d2fc80
Mansidak Singh <mansidaknoor@gmail.com>
2023-02-27 18:48:28 -0700

    the one before this.

--- pages/beta.py
+++ pages/beta.py
@@ -419,7 +419,7 @@ text-align: center;
         with col3a:
             st.write("")
 
-        if ExperienceLevel is not None and Search:
+        if Search:
             Name, newJobtitles, newSkills, softSkills = openAIGetRelevantJobTitles(undesired, resumeContent)
             if 'Name' not in st.session_state:
                 st.session_state['Name'] = Name
