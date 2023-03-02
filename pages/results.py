8d9e5694c46212583266c187132de74e7c864075
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-02 13:14:08 -0700

    the one before this.

--- pages/results.py
+++ pages/results.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="19th Street | Resulsts", page_icon="⓵⓽", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="19th Street | Resulsts", page_icon="⓵⓽", initial_sidebar_state="collapsed", layout="Wide")
 # st.title("CoverLetter")
 import openai
 from docx import Document
