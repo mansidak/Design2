0efa381f6686ebee2bcc91fc6de04617d9a2af44
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-03 11:59:07 -0700

    fef

--- pages/resumebuilder.py
+++ pages/resumebuilder.py
@@ -433,8 +433,8 @@ with tab5:
 
         html_string = ""
 
-        html_string += "<p><font font-size='30' color='#000000'><b>" + CandidateName + "</font></b>"
-        html_string += "<p style = 'size:50px;' >" + CandidateName + "</p>"
+        html_string += "<h1 style='font-size:30px;align:center'>" + CandidateName + "</h1>"
+        html_string += "<p><font size='32' color='#0000FF'>" + CandidateName + "</font></p>"
         html_string += "<h3 style='font-size:15px;align:center'>" + CandidateName + ' | ' + CandidatePhone + ' | ' + CandidateEmail + "</h3>"
 
         html_string += "<h3>" + Experience1Name + "</h3>"
