80eba3649d4047152debee8bed413869864b9ed4
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-02 20:47:19 -0700

    the one before this.

--- pages/results.py
+++ pages/results.py
@@ -372,7 +372,7 @@ with colresult2:
 
             st.markdown("<hr style = 'margin-top:-5px;'>", unsafe_allow_html=True)
 
-    html_string = "<ul>"
+    html_string = "<img src='Favicon.png'><ul>"
     for list in unique_results:
         link = list[0]
         title = list[1]
