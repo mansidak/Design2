271445e6395548ce128f8b38430f0a6de8d5e5ab
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-03 11:44:03 -0700

    Oh Man

--- pages/resumebuilder.py
+++ pages/resumebuilder.py
@@ -433,8 +433,8 @@ with tab5:
 
         html_string = ""
 
-        html_string += "<h1 style='font-size:30px;align:center'>" + CandidateName + "</h1>"
-        html_string += "<h3 style='font-size:150px;align:center'>" + CandidateName + ' | ' + CandidatePhone + ' | ' + CandidateEmail + "</h3>"
+        html_string += "<span style='font-size:30px;align:center'>" + CandidateName + "</span>"
+        html_string += "<span style='font-size:15px;align:center'>" + CandidateName + ' | ' + CandidatePhone + ' | ' + CandidateEmail + "</span>"
 
         html_string += "<h3>" + Experience1Name + "</h3>"
         for item in NewExperienceOneDescription.split(";"):
