f48094979091bedf2aa97f1ebcf104407945f136
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-03 09:44:42 -0700

    Oh Man

--- pages/resumebuilder.py
+++ pages/resumebuilder.py
@@ -359,7 +359,7 @@ with tab5:
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system",
-                 "content": f"You are an AI Assistant that takes in the experience of a job seeker and make it sound like a experiences {ChosenJobTitle}. Your response consists of points separated by semi colon"},
+                 "content": f"You are an AI Assistant that takes in the experience of a job seeker and make it sound like a experienced {ChosenJobTitle} from a first person-point of view. Your response consists of resume points separated by semi colon. You don't add extra fluff to your response"},
                 {"role": "user",
                  "content": f"The following is description of experience of a job seeker.\n{Experience1Description}"}])
         NewExperienceOneDescription = NewExperienceOneDescriptionResponse["choices"][0]["message"]["content"]
@@ -370,7 +370,7 @@ with tab5:
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system",
-                 "content": f"You are an AI Assistant that takes in the experience of a job seeker and make it sound like a experiences {ChosenJobTitle}. Your response consists of points separated by semi colon"},
+                 "content": f"You are an AI Assistant that takes in the experience of a job seeker and make it sound like a experienced {ChosenJobTitle} from a first person-point of view. Your response consists of resume points separated by semi colon. You don't add extra fluff to your response"},
                 {"role": "user",
                  "content": f"The following is description of experience of a job seeker.\n{Experience2Description}"}])
         NewExperienceTwoDescription = NewExperienceTwoDescriptionResponse["choices"][0]["message"]["content"]
@@ -381,7 +381,7 @@ with tab5:
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system",
-                 "content": f"You are an AI Assistant that takes in the experience of a job seeker and make it sound like a experiences {ChosenJobTitle}. Your response consists of points separated by semi colon"},
+                 "content": f"You are an AI Assistant that takes in the experience of a job seeker and make it sound like a experienced {ChosenJobTitle} from a first person-point of view. Your response consists of resume points separated by semi colon. You don't add extra fluff to your response"},
                 {"role": "user",
                  "content": f"The following is description of experience of a job seeker.\n{Experience3Description}"}])
         NewExperienceThreeDescription = NewExperienceThreeDescriptionResponse["choices"][0]["message"]["content"]
@@ -392,7 +392,7 @@ with tab5:
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system",
-                 "content": f"You are an AI Assistant that takes in the experience of a job seeker and make it sound like a experiences {ChosenJobTitle}. Your response consists of points separated by semi colon"},
+                 "content": f"You are an AI Assistant that takes in the experience of a job seeker and make it sound like a experienced {ChosenJobTitle} from a first person-point of view. Your response consists of resume points separated by semi colon. You don't add extra fluff to your response"},
                 {"role": "user",
                  "content": f"The following is description of experience of a job seeker.\n{Experience4Description}"}])
         NewExperienceFourDescription = NewExperienceFourDescriptionResponse["choices"][0]["message"]["content"]
