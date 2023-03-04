556b23f5d604e15be92789ff4ad5d04c62df230e
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-04 15:53:37 -0700

    fef

--- pages/results.py
+++ pages/results.py
@@ -205,7 +205,7 @@ with colresult2:
                         db = firebase.database()
                         user = st.session_state['user']
                         data = {
-                            str(title) : {
+                            str(title + link) : {
                                 "Link": str(link),
                                 "Title": str(title),
                                 "Company Name": str(companyName),
