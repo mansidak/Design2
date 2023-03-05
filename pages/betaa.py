9d6724351d4676881c476f5cdd007fcfde0decd9
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-05 15:34:50 -0700

    the ones

--- pages/betaa.py
+++ pages/betaa.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
         cookie = st.text_input("Cookie", key="1")
         val = st.text_input("Value")
         if st.button("Add"):
-            cookie_manager.set(cookie, val, expires_at=datetime.datetime(year=2022, month=2, day=2))
+            cookie_manager.set(cookie, val, expires_at=datetime.datetime(year=2024, month=2, day=2))
     with c3:
         st.subheader("Delete Cookie:")
         cookie = st.text_input("Cookie", key="2")
