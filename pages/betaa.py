a4dce6bffef2a0ace7be407bb1cee5e3f5934544
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-05 15:45:51 -0700

    the onesf

--- pages/betaa.py
+++ pages/betaa.py
@@ -125,9 +125,9 @@ if __name__ == "__main__":
         st.write(user["localId"])
         st.write(user["email"])
         st.subheader("Set Cookie:")
-        cookie = st.text_input("Cookie", key="not1")
+        cookie = st.text_input("Cookie", key="not1", valye = "UserIDCookie")
         val = st.text_input("Value", value=user)
-        if st.button("Add"):
+        if st.button("Add", key = "notadd"):
             cookie_manager.set(cookie, val, expires_at=datetime.datetime(year=2024, month=2, day=2))
 
 
