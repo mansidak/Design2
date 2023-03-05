cfe385e61b68b44b75621707fd7d3b031d595ff9
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-04 19:25:30 -0700

    fef

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -48,6 +48,46 @@ for key, value in SavedResults.items():
     st.write(Skills)
     Title = value['Title']
     st.write(Title)
+
+    with st.expander(f"{Location}"):
+        st.markdown(f"[Apply]({Link})")
+        st.write(f"{Short_Summary}")
+
+        col1, col2, col3 = st.columns([1, 1, 1])
+
+        with col1:
+            container_2 = st.empty()
+            button_A = container_2.button('Generate Cover Letter', key=f"{Link}+{Title}+{Short_Summary}")
+            if button_A:
+                container_2.empty()
+                button_B = container_2.button('Generating... Please wait.',
+                                              key=f"{Link}+{Title}+{Short_Summary}+Generating", disabled=True)
+                responseJob = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system",
+                         "content": "You are an AI Assistant that summarizes job postings in less than a paragraph."},
+                        {"role": "user",
+                         "content": f"The following is a job posting I want you to summarize \n\n{Full_Description}\n\n"}])
+
+                jobSummary = responseJob["choices"][0]["message"]["content"]
+                CoverLetterResponse = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system",
+                         "content": "You are an AI Assistant that writes highly customized cover letters from a first-person point of view. I have a cover letter format for you:\n\nFirst paragraph: Write about why the candidate is applying to this job. give one of the candidate's skills and relate it to the job requirements. Then give another skill of the job candidate and relate it to the job requirements. \n\nSecond Paragraph: Pick candidate's strongest skills and elaborate on it giving exmaples of their past experiences. Write at least 100 words. Make sure to relate it to the job description\n\nThird Paragraph:  Pick candidate's second strongest skills and elaborate on it giving exmaples of their past experiences. Write at least 100 words. Make sure to relate it to the job description\n\nFourth Paragraph: Conclude with how the candidate is excited to be able to contribute to the job and the company and grow more in a very mature way. "},
+                        {"role": "user",
+                         "content": f"Here's the job description:\n{jobSummary}\n\nHere's the resume data content:\n\n {st.session_state['resumeContent']}"}])
+                cover_letter_file = CoverLetterResponse["choices"][0]["message"]["content"]
+                st.download_button('Download Cover Letter', cover_letter_file)
+
+        with col2:
+            st.write("")
+
+        with col3:
+            st.write("")
+
+    st.markdown("<hr style = 'margin-top:-5px;'>", unsafe_allow_html=True)
 # for item in SavedResults:
 #     for key in item.keys():
 #         company_name = item[key]["Company Name"]
