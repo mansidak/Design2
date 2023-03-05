25458c1cdf4ca05868a45d399183846c0303dbe3
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-04 23:03:07 -0700

    the one

--- pages/betaa.py
+++ pages/betaa.py
@@ -178,29 +178,29 @@ if __name__ == "__main__":
             """, unsafe_allow_html=True)
 
         footer = """<style>
-    a:link , a:visited{
-    color: blue;
-    background-color: transparent;
-    text-decoration: underline;
-    }
-    a:hover,  a:active {
-    color: red;
-    background-color: transparent;
-    text-decoration: underline;
-    }
-    .footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: 0d0d0d;
-    color: 2A2A2A;
-    text-align: center;
-    }
-    </style>
-    <div class="footer" font-family: Sans-Serif;font-weight: lighter;>
-    <p>A Mansidak Singh Production</p>
-    </div>
+                a:link , a:visited{
+                color: blue;
+                background-color: transparent;
+                text-decoration: underline;
+                }
+                a:hover,  a:active {
+                color: red;
+                background-color: transparent;
+                text-decoration: underline;
+                }
+                .footer {
+                position: fixed;
+                left: 0;
+                bottom: 0;
+                width: 100%;
+                background-color: 0d0d0d;
+                color: 2A2A2A;
+                text-align: center;
+                }
+                </style>
+                <div class="footer" font-family: Sans-Serif;font-weight: lighter;>
+                <p>A Mansidak Singh Production</p>
+                </div>
     """
         st.markdown(footer, unsafe_allow_html=True)
 
