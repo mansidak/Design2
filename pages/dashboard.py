642dc8cc705d975629a9e52975556146e1391956
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-06 09:58:48 -0700

    fefew

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -201,7 +201,7 @@ if __name__ == "__main__":
 
 
          #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div:nth-child(7){
-            margin-top:150px;
+            margin-top:0px;
             background-color:#eeeeee;
             min-width:100%;
             min-height:100%;
