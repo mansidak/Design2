cbfef96f7bf812fcf1167e3a9a61d890c0036a96
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-06 09:27:15 -0700

    few

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -194,7 +194,7 @@ if __name__ == "__main__":
         z-index:1;
         
         }
-        div[data-testid="stVerticalBlock"]:nth-child(0){
+        div[data-testid="stVerticalBlock"]:nth-child(6){
             margin-top:-60px;
             margin-bottom:60px;
             }
