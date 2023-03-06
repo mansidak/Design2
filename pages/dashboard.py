4d1b289f6c2cfbead4c946f925bdd4001b7a8cd1
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-06 09:51:46 -0700

    few

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -198,7 +198,7 @@ if __name__ == "__main__":
         div[data-testid="stHeader"] {
         position: relative;
         height:0px;
-        visbility:hidden
+        visibility:hidden
         }
 
          #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div:nth-child(7){
