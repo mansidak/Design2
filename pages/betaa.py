804bf664486ab25f36498ec2e86aabc76c0617ac
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-06 10:10:48 -0700

    fefew

--- pages/betaa.py
+++ pages/betaa.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
                 }
                 #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div:nth-child(4){
                     margin-top:-90px;
-                    margin-left:-70px;
+                    margin-left:-90px;
                     min-width:100%;
                     position:fixed;
                     z-index:1;
