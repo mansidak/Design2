70290376b5ad1dab71fc3f34a7887da4d83c1171
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-05 19:05:40 -0700

    the onesf

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,6 +81,7 @@ st.markdown(
             
             .css-1db87p3.edgvbvh10{
                     padding-top: 0;
+                    padding-right:0;
                   }
                   
                   
