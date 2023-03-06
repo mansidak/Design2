e4b3e7756b386754ec665d8bfd0dc047f9f2b141
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-06 09:43:16 -0700

    few

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -197,12 +197,8 @@ if __name__ == "__main__":
          #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div:nth-child(7){
             margin-top:-150px;
             background-color:#eeeeee;
-         
+            position:fixed
             }
-        #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div:nth-child(7) > div.css-keje6w.e1tzin5v2{
-        background-color:#eeeeee;
-           position:absolute
-            }       
        
           .dark{
                 background-color: #eeeeee;
