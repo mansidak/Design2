9387dc2b49c31d933bf395031ebbe24b1644a55f
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-06 09:34:07 -0700

    few

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
              
         .css-1uhah0b.e8zbici2{
         z-index:0;
-        position:absolute;
+        position:fixed;
         }
          #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div:nth-child(7){
             margin-top:-150px;
