214c7205ebc72bff95ff1afa5639102bc35fdb53
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-05 21:50:11 -0700

    the onesf

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -199,6 +199,7 @@ if __name__ == "__main__":
                 unsafe_allow_html=True)
             st.markdown(f"<h5 style='text-align:center; font-weight:lighter;color:black'>You're logged in as {st.session_state['user']['email']}</h5>", unsafe_allow_html=True)
             if FirebaseResumeContent:
+                st.write("Resume on file:✅")
                 colResumeSub1, colResumeSub2,colResumeSub3, colResumeSub4  = st.columns([ 0.25,1.3,1,0.25])
                 with colResumeSub2:
                     if st.button("Remove Resume on File"):
