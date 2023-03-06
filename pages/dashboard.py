b5e1a81b18b0bd36aaee20eafa0970c26206d082
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-06 08:11:35 -0700

    the onesf

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
             stx.TabBarItemData(id=2, title="Done", description="Tasks taken care of"),
             stx.TabBarItemData(id=3, title="Overdue", description="Tasks missed out"),
         ], default=2)
-        if chosen_id == 1:
+        if chosen_id.id == 1:
             switch_page("betaa")
 
 
