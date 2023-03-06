6388bc334e628ef1d58474432a7a28490c67e59f
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-06 10:00:38 -0700

    fefew

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -171,7 +171,7 @@ if __name__ == "__main__":
                                default_index=3, orientation="horizontal",
                                 styles={
                                         "container": {"padding": "0!important", "background-color": "#0f0f0f"},
-                                        "nav-link": {"font-size": "15px", "text-align": "center", "margin": "0px", "--hover-color": "#eee", "color":"black","background-color": "#eeeeee"},
+                                        "nav-link": {"font-size": "15px", "text-align": "center", "margin": "0px", "--hover-color": "#eee", "color":"black","background-color": "#0f0f0f"},
                                         "nav-link-selected": {"font-weight": "bold", "background-color":"#eeeeee", "color":"black"},
                                     })
 
