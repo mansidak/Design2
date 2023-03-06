fe2e7a4f5b604499a9cf4d0b28225cc6defb2d6c
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-06 10:21:28 -0700

    fefew

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
         with coldash2:
             selected2 = option_menu(None, ["Home", "Search", "Build", 'Dashboard'],
                                     icons=['house', 'cloud-upload', "list-task", 'gear'],
-                                    menu_icon="cast", default_index=0, orientation="horizontal",
+                                    menu_icon="cast", default_index=3, orientation="horizontal",
                                 styles={
                                         "container": {"padding": "0!important", "background-color": "#0f0f0f"},
                                         "nav-link": {"font-size": "15px", "text-align": "center", "margin": "0px", "--hover-color": "#0f0f0f", "color": "white", "background-color": "#0f0f0f"},
