166282d92fa37fc115d54a5b2832345e07a001a6
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-06 08:19:00 -0700

    the onesf

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
                     switch_page("dashboard")
         selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],
                                 icons=['house', 'cloud-upload', "list-task", 'gear'],
-                                menu_icon="cast", default_index=0, orientation="horizontal")
+                                menu_icon="cast", default_index=0, orientation="horizontal", nav =True)
         selected2
 
 
