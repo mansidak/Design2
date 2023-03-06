a4d2a7e646183191364bee6a6c2b1027033c5fef
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-05 19:34:14 -0700

    the onesf

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -67,7 +67,7 @@ hide_streamlit_style = """
                 width: 50%;
               }
 
-                .stApp {
+                [data-testid="stAppViewContainer"'] {
                 background-color: #eeeeee
                 }
         
