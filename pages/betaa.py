5f51663f5344634140d9ad71ecd5d6baf0784fd5
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-06 20:58:53 -0700

    dfsdf

--- pages/betaa.py
+++ pages/betaa.py
@@ -614,7 +614,7 @@ if __name__ == "__main__":
                 # st.write(OldSkillsBullet)
                 # st.write(Matches)
                 MatchTuples = MatchMethod(Matches)
-                st.write(MatchTuples[0])
+                st.write(MatchTuples.type())
 
 
                 holder2 = st.empty()
