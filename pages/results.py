7e25e26c05cad178a4d71f170ed26cb42d72c3a7
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-07 08:08:11 -0700

    dfsdf

--- pages/results.py
+++ pages/results.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title="19th Street | Resulsts", page_icon="⓵⓽", initial_sidebar_state="expanded", layout="wide")
 # st.title("CoverLetter")
