1e13a9755209bd4cffb81cde23220551c15ebb56
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-08 10:00:04 -0700

    dfsdf

--- pages/signup.py
+++ pages/signup.py
@@ -48,11 +48,12 @@ def login_form(auth):
 
     if st.button("login"):
         try:
-            user = auth.sign_in_with_email_and_password(email, password)
+            user = auth.create_user_with_email_and_password(email, password)
             st.session_state['user'] = user
             st.experimental_rerun()
         except requests.HTTPError as exception:
             st.write(exception)
+        switch_page("dashboard")
 
 
 def logout():
