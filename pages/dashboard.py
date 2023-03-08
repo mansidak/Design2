3cd1b45680e198346066880796c56b89b37729d4
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-08 10:12:33 -0700

    dfsdf

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -250,6 +250,27 @@ if __name__ == "__main__":
         st.markdown(
             f"<h5 style='text-align:center; font-weight:lighter;color:black'>You're logged in as {st.session_state['user']['email']}</h5>",
             unsafe_allow_html=True)
+        c1, c2, c3 = st.columns(3)
+        with c1:
+            st.subheader("Get Cookie:")
+            cookie = st.text_input("Cookie", key="0")
+            clicked = st.button("Get")
+            if clicked:
+                value = cookie_manager.get(cookie=cookie)
+                st.write(value)
+        with c2:
+            st.subheader("Set Cookie:")
+            cookie = st.text_input("Cookie", key="1")
+            val = st.text_input("Value")
+            if st.button("Add"):
+                cookie_manager.set(cookie, val, expires_at=datetime.datetime(year=2022, month=2, day=2))
+        with c3:
+            st.subheader("Delete Cookie:")
+            cookie = st.text_input("Cookie", key="2")
+            if st.button("Delete"):
+                cookie_manager.delete(cookie)
+
+
         cookies = cookie_manager.get_all()
         st.write(cookies)
 
