31f6a3319be299a0cc3663b6f0157ce8e0acccb1
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-08 23:09:03 -0700

    AJAAA

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -244,7 +244,7 @@ if __name__ == "__main__":
 
 
         with st.sidebar:
-            ArchivedResults = db.child("users").child(str(localId)).child("Archive").child("Archive1")get().val()
+            ArchivedResults = db.child("users").child(str(localId)).child("Archive").child("Archive1").get().val()
             unique_links = {}
 
             for key, value in SavedResults.items():
