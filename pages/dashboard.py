3ca0ade890fa7eea91153e594bceb4e54eace3d5
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-10 11:42:57 -0700

    AJAAA

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -305,6 +305,7 @@ if __name__ == "__main__":
     background-color:#ffffff;
     color:black;
     border-radius:5px;
+    height:10px !important;
     min-width:150px;
     padding:20px;
     margin: 0 auto;
@@ -316,8 +317,8 @@ if __name__ == "__main__":
     }
     
     
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div.stTabs.css-0.exp6ofz0 > div > div:nth-child(1) > div > button:nth-child(1) {
-        max-height:10px !important;
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div.stTabs.css-0.exp6ofz0 > div > div:nth-child(1) > div > button {
+    height:10px !important;
 
     }
 
