589ceae0b5d86dffd0652c7eb43c2512abb87169
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-10 10:59:03 -0700

    AJAAA

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -318,7 +318,7 @@ if __name__ == "__main__":
     
     div[data-baseweb="tab-list"] {
     background-color:#fefefe;
-    max-width:450px;
+    max-width:480px;
     padding:5px;
     border-bottom:none;
     border-radius:5px;
