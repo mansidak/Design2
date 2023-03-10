57919c79f1f3acdd6a4cbcd14edbcaf82b2b3a42
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-10 11:43:12 -0700

    AJAAA

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -317,10 +317,6 @@ if __name__ == "__main__":
     }
     
     
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div.stTabs.css-0.exp6ofz0 > div > div:nth-child(1) > div > button {
-    height:10px !important;
-
-    }
 
     div[data-baseweb="tab-highlight"] {
     height:0px;
