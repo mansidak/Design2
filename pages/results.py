736bba98edad0d23551af22c31ea47fdb05afef8
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-10 13:24:30 -0700

    AJAAA

--- pages/results.py
+++ pages/results.py
@@ -133,7 +133,7 @@ if __name__ == "__main__":
                                 margin-top:-30px;
                                 }
                                 .css-1w67d01.e1tzin5v0{
-                                margin-top: 10px;
+                                margin-top: 30px;
                                 }
 
 
@@ -590,26 +590,26 @@ if __name__ == "__main__":
         with colresult3:
             st.write("")
 
-        for job in unique_results:
-            firebase = pyrebase.initialize_app(firebaseconfig)
-            db = firebase.database()
-            link = job[0]
-            title = job[1]
-            companyName = job[2]
-            shortSummary = job[3]
-            fullDescription = job[4]
-            location = job[5]
-            skills = job[6]
-            data = {
-                "Link": str(link),
-                "Title": str(title),
-                "Company Name": str(companyName),
-                "Short Summary": str(shortSummary),
-                "Full Description": str(fullDescription),
-                "Location": str(location),
-                "Skills": str(skills)
-            }
-            db.child("users").child(str(localId)).child("Archive").child("Archive1").push(data)
+        # for job in unique_results:
+        #     firebase = pyrebase.initialize_app(firebaseconfig)
+        #     db = firebase.database()
+        #     link = job[0]
+        #     title = job[1]
+        #     companyName = job[2]
+        #     shortSummary = job[3]
+        #     fullDescription = job[4]
+        #     location = job[5]
+        #     skills = job[6]
+        #     data = {
+        #         "Link": str(link),
+        #         "Title": str(title),
+        #         "Company Name": str(companyName),
+        #         "Short Summary": str(shortSummary),
+        #         "Full Description": str(fullDescription),
+        #         "Location": str(location),
+        #         "Skills": str(skills)
+        #     }
+        #     db.child("users").child(str(localId)).child("Archive").child("Archive1").push(data)
 
 
 
