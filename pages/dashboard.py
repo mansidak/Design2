2713cbda5111732332a5e4a712e35d6c9635b125
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-10 12:02:54 -0700

    AJAAA

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -260,6 +260,7 @@ if __name__ == "__main__":
 
                 colResumeSub1, colResumeSub2, colResumeSub3, colResumeSub4 = st.columns([4,0.75,1,4])
                 with colResumeSub2:
+                    st.write("")
                     st.markdown(
                         f"<h6 style='text-align:center; font-weight:lighter;color:black'>Resume on file:<span style='color: green'>&nbsp &check;</span> </h6>",
                         unsafe_allow_html=True)
