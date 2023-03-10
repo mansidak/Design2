64fd9d7809a86830b86d8d5413a0ae9d6a649f00
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-10 11:36:44 -0700

    AJAAA

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -312,8 +312,13 @@ if __name__ == "__main__":
     
     button[data-baseweb="tab"]:hover {
     color:black;
-    max-height:20px !important;
     background-color:#eeeeee;
+    }
+    
+    
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div.stTabs.css-0.exp6ofz0 > div > div:nth-child(1) > div{
+        max-height:20px !important;
+
     }
 
     div[data-baseweb="tab-highlight"] {
