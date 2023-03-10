05843d3a69371c380e24401943a4d86971ecffc3
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-10 08:09:32 -0700

    AJAAA

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -295,6 +295,7 @@ if __name__ == "__main__":
     color:black;
     border-radius:5px;
     min-width:150px;
+    passing:5px;
     margin: 0 auto;
     }
     
@@ -305,7 +306,6 @@ if __name__ == "__main__":
     div[data-baseweb="tab-list"] {
     background-color:#eeeeee;
     max-width:320px;
-    min-height:50px;
     border-radius:5px;
     margin: 0 auto;
     }
