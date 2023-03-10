185eadea38d8972efbca1dd6dceb5f6e0f7245fa
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-10 13:15:04 -0700

    AJAAA

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -268,6 +268,8 @@ if __name__ == "__main__":
                         f"<h6 style='text-align:center; font-weight:lighter;color:black'>Resume on file:<span style='color: green'>&nbsp &check;</span> </h6>",
                         unsafe_allow_html=True)
                 with colResumeSub3:
+                    st.subheader("")
+                    st.subheader("")
                     if st.button("Upload new resume"):
                         db.child("users").child(str(localId)).child("Resume").remove()
                         del st.session_state['resumeContent']
