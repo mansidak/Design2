9d30d08c734f19dd2fbc6c2074baba7c3a5dfeb1
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-10 07:55:52 -0700

    AJAAA

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -167,100 +167,29 @@ if __name__ == "__main__":
             st.write("")
 
 
-        Saved, Archive = st.tabs(["Saved", "Arvice"])
 
-        with Saved:
-            SavedResults = db.child("users").child(str(localId)).child("Jobs").get().val()
-            unique_links = {}
 
-            for key, value in SavedResults.items():
-                link = value['Link']
-                if link not in unique_links:
-                    unique_links[link] = value
 
-            my_dict = unique_links
-            colresult1, colresult2, colresult3 = st.columns([0.25, 1, 0.25])
-            with colresult1:
-                st.write("")
-            with colresult2:
-                for key, value in my_dict.items():
-                    company_name = value['Company Name']
-                    Full_Description = value['Full Description']
-                    Link = value['Link']
-                    Location = value['Location']
-                    Short_Summary = value['Short Summary']
-                    Skills = value['Skills']
-                    Title = value['Title']
+        colSection1, colSection2, colSection3 = st.columns([0.25,1,0.25])
+        with colSection1:
+            st.write("")
+        with colSection2:
+            Saved, Archive = st.tabs(["Saved", "Archive"])
 
-                    st.markdown(
-                        f"<a href='{Link}' style='text-decoration: none; color: black;' target='_blank'><h4 style='font-family: Sans-Serif;margin-top:-20px;'>&nbsp;&nbsp;{Title}→ </h4></a>",
-                        unsafe_allow_html=True)
-                    st.markdown(
-                        f"<h6 style='font-family: Sans-Serif;font-weight: bold;margin-top:-20px;'>&nbsp;&nbsp;&nbsp;{company_name}</h6>",
-                        unsafe_allow_html=True)
-                    with st.expander(f"{Location}"):
-                        st.markdown(f"[Apply]({Link})")
-                        st.write(f"{Short_Summary}")
+            with Saved:
+                SavedResults = db.child("users").child(str(localId)).child("Jobs").get().val()
+                unique_links = {}
 
-                        col1, col2, col3 = st.columns([1, 1, 1])
+                for key, value in SavedResults.items():
+                    link = value['Link']
+                    if link not in unique_links:
+                        unique_links[link] = value
 
-                        with col1:
-                            container_2 = st.empty()
-                            button_A = container_2.button('Generate Cover Letter', key=f"{Link}+{Title}+{Short_Summary}")
-                            if button_A:
-                                container_2.empty()
-                                button_B = container_2.button('Generating... Please wait.',
-                                                              key=f"{Link}+{Title}+{Short_Summary}+Generating",
-                                                              disabled=True)
-                                responseJob = openai.ChatCompletion.create(
-                                    model="gpt-3.5-turbo",
-                                    messages=[
-                                        {"role": "system",
-                                         "content": "You are an AI Assistant that summarizes job postings in less than a paragraph."},
-                                        {"role": "user",
-                                         "content": f"The following is a job posting I want you to summarize \n\n{Full_Description}\n\n"}])
-
-                                jobSummary = responseJob["choices"][0]["message"]["content"]
-                                CoverLetterResponse = openai.ChatCompletion.create(
-                                    model="gpt-3.5-turbo",
-                                    messages=[
-                                        {"role": "system",
-                                         "content": "You are an AI Assistant that writes highly customized cover letters from a first-person point of view. I have a cover letter format for you:\n\nFirst paragraph: Write about why the candidate is applying to this job. give one of the candidate's skills and relate it to the job requirements. Then give another skill of the job candidate and relate it to the job requirements. \n\nSecond Paragraph: Pick candidate's strongest skills and elaborate on it giving exmaples of their past experiences. Write at least 100 words. Make sure to relate it to the job description\n\nThird Paragraph:  Pick candidate's second strongest skills and elaborate on it giving exmaples of their past experiences. Write at least 100 words. Make sure to relate it to the job description\n\nFourth Paragraph: Conclude with how the candidate is excited to be able to contribute to the job and the company and grow more in a very mature way. "},
-                                        {"role": "user",
-                                         "content": f"Here's the job description:\n{jobSummary}\n\nHere's the resume data content:\n\n {st.session_state['resumeContent']}"}])
-                                cover_letter_file = CoverLetterResponse["choices"][0]["message"]["content"]
-                                st.download_button('Download Cover Letter', cover_letter_file)
-
-                        with col2:
-                            st.write("")
-
-                        with col3:
-                            st.write("")
-
-                    st.markdown("<hr  color=black style = 'margin-top:-5px;background-color:black'>", unsafe_allow_html=True)
-            with colresult3:
-                st.write("")
-
-        with Archive:
-            AccountInfo = auth.get_account_info(user['idToken'])["users"][0]
-            firebase = pyrebase.initialize_app(firebaseconfig)
-            db = firebase.database()
-            localId = AccountInfo["localId"]
-            ArchivedResults = db.child("users").child(str(localId)).child("Archive").child("Archive1").get().val()
-
-            unique_links = {}
-
-            for key, value in ArchivedResults.items():
-                link = value['Link']
-                if link not in unique_links:
-                    unique_links[link] = value
-
-            my_dict = unique_links
-            colresult1, colresult2, colresult3 = st.columns([0.25, 1, 0.25])
-            with colresult1:
-                st.write("")
-            with colresult2:
-                with st.expander("Archive1"):
+                my_dict = unique_links
+                colresult1, colresult2, colresult3 = st.columns([0.25, 1, 0.25])
+                with colresult1:
+                    st.write("")
+                with colresult2:
                     for key, value in my_dict.items():
                         company_name = value['Company Name']
                         Full_Description = value['Full Description']
@@ -269,18 +198,100 @@ if __name__ == "__main__":
                         Short_Summary = value['Short Summary']
                         Skills = value['Skills']
                         Title = value['Title']
-                        # with st.expander("Archive1"):
+
                         st.markdown(
-                            f"<a href='{Link}' style='text-decoration: none; color: white;' target='_blank'><h5 style='font-family: Sans-Serif;margin-top:-20px;'>{Title}→ </h5></a>",
+                            f"<a href='{Link}' style='text-decoration: none; color: black;' target='_blank'><h4 style='font-family: Sans-Serif;margin-top:-20px;'>&nbsp;&nbsp;{Title}→ </h4></a>",
                             unsafe_allow_html=True)
                         st.markdown(
-                            f"<h6 style='font-family: Sans-Serif;font-weight: bold;margin-top:-20px; color:white'>{company_name}</h6>",
+                            f"<h6 style='font-family: Sans-Serif;font-weight: bold;margin-top:-20px;'>&nbsp;&nbsp;&nbsp;{company_name}</h6>",
                             unsafe_allow_html=True)
+                        with st.expander(f"{Location}"):
+                            st.markdown(f"[Apply]({Link})")
+                            st.write(f"{Short_Summary}")
+
+                            col1, col2, col3 = st.columns([1, 1, 1])
+
+                            with col1:
+                                container_2 = st.empty()
+                                button_A = container_2.button('Generate Cover Letter',
+                                                              key=f"{Link}+{Title}+{Short_Summary}")
+                                if button_A:
+                                    container_2.empty()
+                                    button_B = container_2.button('Generating... Please wait.',
+                                                                  key=f"{Link}+{Title}+{Short_Summary}+Generating",
+                                                                  disabled=True)
+                                    responseJob = openai.ChatCompletion.create(
+                                        model="gpt-3.5-turbo",
+                                        messages=[
+                                            {"role": "system",
+                                             "content": "You are an AI Assistant that summarizes job postings in less than a paragraph."},
+                                            {"role": "user",
+                                             "content": f"The following is a job posting I want you to summarize \n\n{Full_Description}\n\n"}])
+
+                                    jobSummary = responseJob["choices"][0]["message"]["content"]
+                                    CoverLetterResponse = openai.ChatCompletion.create(
+                                        model="gpt-3.5-turbo",
+                                        messages=[
+                                            {"role": "system",
+                                             "content": "You are an AI Assistant that writes highly customized cover letters from a first-person point of view. I have a cover letter format for you:\n\nFirst paragraph: Write about why the candidate is applying to this job. give one of the candidate's skills and relate it to the job requirements. Then give another skill of the job candidate and relate it to the job requirements. \n\nSecond Paragraph: Pick candidate's strongest skills and elaborate on it giving exmaples of their past experiences. Write at least 100 words. Make sure to relate it to the job description\n\nThird Paragraph:  Pick candidate's second strongest skills and elaborate on it giving exmaples of their past experiences. Write at least 100 words. Make sure to relate it to the job description\n\nFourth Paragraph: Conclude with how the candidate is excited to be able to contribute to the job and the company and grow more in a very mature way. "},
+                                            {"role": "user",
+                                             "content": f"Here's the job description:\n{jobSummary}\n\nHere's the resume data content:\n\n {st.session_state['resumeContent']}"}])
+                                    cover_letter_file = CoverLetterResponse["choices"][0]["message"]["content"]
+                                    st.download_button('Download Cover Letter', cover_letter_file)
+
+                            with col2:
+                                st.write("")
+
+                            with col3:
+                                st.write("")
+
                         st.markdown("<hr  color=black style = 'margin-top:-5px;background-color:black'>",
                                     unsafe_allow_html=True)
-            with colresult3:
-                st.write("")
+                with colresult3:
+                    st.write("")
 
+            with Archive:
+                AccountInfo = auth.get_account_info(user['idToken'])["users"][0]
+                firebase = pyrebase.initialize_app(firebaseconfig)
+                db = firebase.database()
+                localId = AccountInfo["localId"]
+                ArchivedResults = db.child("users").child(str(localId)).child("Archive").child("Archive1").get().val()
+
+                unique_links = {}
+
+                for key, value in ArchivedResults.items():
+                    link = value['Link']
+                    if link not in unique_links:
+                        unique_links[link] = value
+
+                my_dict = unique_links
+                colresult1, colresult2, colresult3 = st.columns([0.25, 1, 0.25])
+                with colresult1:
+                    st.write("")
+                with colresult2:
+                    with st.expander("Archive1"):
+                        for key, value in my_dict.items():
+                            company_name = value['Company Name']
+                            Full_Description = value['Full Description']
+                            Link = value['Link']
+                            Location = value['Location']
+                            Short_Summary = value['Short Summary']
+                            Skills = value['Skills']
+                            Title = value['Title']
+                            # with st.expander("Archive1"):
+                            st.markdown(
+                                f"<a href='{Link}' style='text-decoration: none; color: white;' target='_blank'><h5 style='font-family: Sans-Serif;margin-top:-20px;'>{Title}→ </h5></a>",
+                                unsafe_allow_html=True)
+                            st.markdown(
+                                f"<h6 style='font-family: Sans-Serif;font-weight: bold;margin-top:-20px; color:white'>{company_name}</h6>",
+                                unsafe_allow_html=True)
+                            st.markdown("<hr  color=black style = 'margin-top:-5px;background-color:black'>",
+                                        unsafe_allow_html=True)
+                with colresult3:
+                    st.write("")
+
+        with colSection3:
+            st.write("")
 
 
     st.markdown("""
