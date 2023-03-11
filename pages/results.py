074c5b922aca444bcc8e2845cc9cdc6f4f2b523f
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-11 15:38:10 -0700

    the one before this and the ones before that works .

--- pages/results.py
+++ pages/results.py
@@ -523,11 +523,11 @@ if __name__ == "__main__":
                             Save.empty()
 
                     with st.expander(f"{location}"):
-                        st.markdown(f"[Apply]({link})")
-                        st.write(f"{shortSummary}")
                         score_text = compatibilityScore.split('Score: ')[1].split(';')[0]
                         skills_text = compatibilityScore.split('Skills that match: ')[1]
                         st.metric("Compatibility Score", f"{score_text}%", f"{skills_text}")
+                        st.markdown(f"[Apply]({link})")
+                        st.write(f"{shortSummary}")
 
                         col1, col2, col3 = st.columns([1, 1, 1])
 
