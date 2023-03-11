ed833bf7691981fcd76b8753e8f7a159f9182f0b
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-11 16:51:02 -0700

    the one before this and the ones before that works .

--- pages/results.py
+++ pages/results.py
@@ -631,26 +631,26 @@ if __name__ == "__main__":
             st.write("")
 
         st.subheader(datetime.datetime.now())
-        # for job in unique_results:
-        #     firebase = pyrebase.initialize_app(firebaseconfig)
-        #     db = firebase.database()
-        #     link = job[0]
-        #     title = job[1]
-        #     companyName = job[2]
-        #     shortSummary = job[3]
-        #     fullDescription = job[4]
-        #     location = job[5]
-        #     skills = job[6]
-        #     data = {
-        #         "Link": str(link),
-        #         "Title": str(title),
-        #         "Company Name": str(companyName),
-        #         "Short Summary": str(shortSummary),
-        #         "Full Description": str(fullDescription),
-        #         "Location": str(location),
-        #         "Skills": str(skills)
-        #     }
-        #     db.child("users").child(str(localId)).child("Archive").child("Archive1").push(data)
+        for job in unique_results:
+            firebase = pyrebase.initialize_app(firebaseconfig)
+            db = firebase.database()
+            link = job[0]
+            title = job[1]
+            companyName = job[2]
+            shortSummary = job[3]
+            fullDescription = job[4]
+            location = job[5]
+            skills = job[6]
+            data = {
+                "Link": str(link),
+                "Title": str(title),
+                "Company Name": str(companyName),
+                "Short Summary": str(shortSummary),
+                "Full Description": str(fullDescription),
+                "Location": str(location),
+                "Skills": str(skills)
+            }
+            db.child("users").child(str(localId)).child("Archive").child(datetime.datetime.now()).push(data)
 
 
 
