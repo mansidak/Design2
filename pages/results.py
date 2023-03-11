d0367ca236b3160e862eb0a2aff27b0882472216
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-11 16:34:46 -0700

    the one before this and the ones before that works .

--- pages/results.py
+++ pages/results.py
@@ -149,7 +149,7 @@ if __name__ == "__main__":
                                 #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div:nth-child(8) {
                                     margin-top:-90px;
                                     margin-left:-90px;
-                                    min-width:200%;
+                                    min-width:100%;
                                     position:fixed;
                                     z-index:1;
                                     }
