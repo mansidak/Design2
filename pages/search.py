327773805111198332999f0dbed8d4dd440e9e8d
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-12 13:00:14 -0700

    the one before this and the ones before that works .

--- pages/search.py
+++ pages/search.py
@@ -287,11 +287,19 @@ if __name__ == "__main__":
 
             progress_text = "See your search progress here."
             progress_text_2 = "Hola"
-
+            st.markdown("""
+                                     <style>
+                                     .stProgress .st-bo {
+                                     background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+                                     }
+                                     </style>
+                                     """, unsafe_allow_html=True)
             hide_menu_style = """
             
             
                                     <style>
+                                    
+                                    
                                     #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div:nth-child(11){
             margin-top:-120px;
             }
@@ -1029,13 +1037,7 @@ if __name__ == "__main__":
 
 
                             def progress_shit():
-                                st.markdown("""
-                                <style>
-                                .stProgress .st-bo {
-                                background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-                                }
-                                </style>
-                                """, unsafe_allow_html=True)
+
 
                                 progressText.markdown( f"<h6 style='text-align: center; font-family: Sans-Serif;font-weight: lighter;'>Looking for jobs where you can use your experience in {st.session_state['newSkills']} etc...</h6>", unsafe_allow_html=True)
                                 my_bar.progress(25, text=f"")
