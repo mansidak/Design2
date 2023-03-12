2ebe3c2791a569c0de249849fd70fbdb4868ce3a
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-12 13:12:08 -0700

    the one before this and the ones before that works .

--- pages/search.py
+++ pages/search.py
@@ -1115,18 +1115,12 @@ if __name__ == "__main__":
                                 link = job[0]
                                 title = job[1]
                                 companyName = job[2]
-                                shortSummary = job[3]
-                                fullDescription = job[4]
                                 location = job[5]
-                                skills = job[6]
                                 data = {
                                     "Link": str(link),
                                     "Title": str(title),
                                     "Company Name": str(companyName),
-                                    "Short Summary": str(shortSummary),
-                                    "Full Description": str(fullDescription),
                                     "Location": str(location),
-                                    "Skills": str(skills),
                                 }
 
                                 db.child("users").child(str(localId)).child("Archive").push(data)
