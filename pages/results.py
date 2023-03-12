a07091385a622903dfd3a56922544e099782e19c
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-12 11:04:14 -0700

    the one before this and the ones before that works .

--- pages/results.py
+++ pages/results.py
@@ -637,7 +637,7 @@ if __name__ == "__main__":
                 with open("output.pdf", "rb") as pdf_file:
                     PDFbyte = pdf_file.read()
                 # st.download_button(label='Download PDF', data= PDFFile)
-                st.subheader("")
+                st.write("")
                 st.write("")
                 st.download_button(label="Download All Jobs",
                                    data=PDFbyte,
