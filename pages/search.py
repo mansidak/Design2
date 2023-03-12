28448bbb3b7459f4689563bfbf1e80d2e4dd35fb
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-11 20:43:01 -0700

    the one before this and the ones before that works .

--- pages/search.py
+++ pages/search.py
@@ -1069,6 +1069,28 @@ if __name__ == "__main__":
                     st.session_state["FinalResults"] = links1 + links2 + links3 + links4 + links5
                     Archives = links1 + links2 + links3 + links4 + links5
 
+                    for job in Archives:
+                        firebase = pyrebase.initialize_app(firebaseconfig)
+                        db = firebase.database()
+                        link = job[0]
+                        title = job[1]
+                        companyName = job[2]
+                        shortSummary = job[3]
+                        fullDescription = job[4]
+                        location = job[5]
+                        skills = job[6]
+                        data = {
+                            "Link": str(link),
+                            "Title": str(title),
+                            "Company Name": str(companyName),
+                            "Short Summary": str(shortSummary),
+                            "Full Description": str(fullDescription),
+                            "Location": str(location),
+                            "Skills": str(skills),
+                        }
+
+                        db.child("users").child(str(localId)).child("Archive").push(data)
+                    st.subheader(datetime.datetime.now())
 
 
                     switch_page("results")
@@ -1076,28 +1098,7 @@ if __name__ == "__main__":
             st.write("")
 
 
-        for job in Archives:
-            firebase = pyrebase.initialize_app(firebaseconfig)
-            db = firebase.database()
-            link = job[0]
-            title = job[1]
-            companyName = job[2]
-            shortSummary = job[3]
-            fullDescription = job[4]
-            location = job[5]
-            skills = job[6]
-            data = {
-                "Link": str(link),
-                "Title": str(title),
-                "Company Name": str(companyName),
-                "Short Summary": str(shortSummary),
-                "Full Description": str(fullDescription),
-                "Location": str(location),
-                "Skills": str(skills),
-            }
 
-            db.child("users").child(str(localId)).child("Archive").push(data)
-        st.subheader(datetime.datetime.now())
 
 
     hide_menu_style = """
