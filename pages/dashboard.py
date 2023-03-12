3cab9666024ef711e39d07a426c91e767268fd8b
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-12 09:07:55 -0700

    the one before this and the ones before that works .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -375,7 +375,7 @@ if __name__ == "__main__":
      
      button[data-baseweb="tab"]{
      display:block;
-     max-height:25px;
+     max-height:20px;
      } 
 
     div[data-baseweb="tab-list"] {
