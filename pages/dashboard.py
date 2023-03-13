ce10cbc15e0a91edc7e134e5065a8475623a3d06
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-13 06:56:12 -0700

    the one before this and the ones before that works .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -166,7 +166,7 @@ if __name__ == "__main__":
                 st.write("")
             SavedResults = db.child("users").child(str(localId)).child("Jobs").get().val()
             unique_links = {}
-            with st.form("Dashboard"):
+            with st.form("Dashboard", key= "SAveDjobs"):
                 for key, value in SavedResults.items():
                     link = value['Link']
                     if link not in unique_links:
