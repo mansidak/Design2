d73c32a014e17da5f14c14cbe3dad933c324c29f
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-13 08:30:05 -0700

    the one before this and the ones before that works .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -8,8 +8,6 @@ from PIL import Image
 from streamlit_option_menu import option_menu
 import PyPDF2
 from streamlit_card import card
-import streamlit as st
-from streamlit_modal import Modal
 import random
 from streamlit_extras.switch_page_button import switch_page
 import pdfkit
@@ -140,33 +138,13 @@ if __name__ == "__main__":
 
         st.session_state['Name'] = db.child('users').child(localId).child('Name').get().val()
         st.markdown(
-            f"<center> <h1 style='font-family: Sans-Serif; font-weight:normal; color: white'>{st.session_state['Name'].replace('.','')}'s Dashboard</h1>",
+            f"<h1 style='font-family: w3-sans-serif; font-weight:normal; color: white'>{st.session_state['Name'].replace('.','')}'s Street</h1>",
             unsafe_allow_html=True)
 
         Saved, Archive, ResumeTab = st.tabs(["Saved", "Archive", "Profile"])
 
         with Saved:
-            modal = Modal("Demo Modal", key = "Modal")
-            open_modal = st.button("Open")
-            if open_modal:
-                modal.open()
 
-            if modal.is_open():
-                with modal.container():
-                    st.write("Text goes here")
-
-                    html_string = '''
-                    <h1>HTML string in RED</h1>
-
-                    <script language="javascript">
-                      document.querySelector("h1").style.color = "red";
-                    </script>
-                    '''
-                    components.html(html_string)
-
-                    st.write("Some fancy text")
-                    value = st.checkbox("Check me")
-                    st.write(f"Checkbox checked: {value}")
 
             colLogin1, colLogin2, colLogin3 = st.columns([2, 1, 2])
             with colLogin2:
