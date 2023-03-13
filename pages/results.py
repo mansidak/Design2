98662ab4dbf59785f8ee493dcfff04c2a2da0b7e
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-12 20:13:14 -0700

    the one before this and the ones before that works .

--- pages/results.py
+++ pages/results.py
@@ -431,45 +431,9 @@ if __name__ == "__main__":
             }
             
             
-            /* Pseudo element for check styling */
-            
-            input[type=checkbox]::before {
-              content: "";
-              color: transparent;
-              display: block;
-              width: inherit;
-              height: inherit;
-              border-radius: inherit;
-              border: 0;
-              background-color: transparent;
-              background-size: contain;
-              box-shadow: inset 0 0 0 1px #CCD3D8;
-            }
-            
-            
-            /* Checked */
-            
-            input[type=checkbox]:checked {
-              background-color: currentcolor;
-            }
-            
-            input[type=checkbox]:checked::before {
-              box-shadow: none;
-              background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E %3Cpath d='M15.88 8.29L10 14.17l-1.88-1.88a.996.996 0 1 0-1.41 1.41l2.59 2.59c.39.39 1.02.39 1.41 0L17.3 9.7a.996.996 0 0 0 0-1.41c-.39-.39-1.03-.39-1.42 0z' fill='%23fff'/%3E %3C/svg%3E");
-            }
-            
-            
-            /* Disabled */
-            
-            input[type=checkbox]:disabled {
-              background-color: #CCD3D8;
-              opacity: 0.84;
-              cursor: not-allowed;
-            }
-
-            
             </style>
             """, unsafe_allow_html=True)
+
             colconclusion1, colconclusion2 = st.columns([1, 3])
             with colconclusion1:
 
