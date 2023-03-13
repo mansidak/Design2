c58f0f4c4d5fa01bafee3efc30d5ea175c621879
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-13 13:33:13 -0700

    the one before this and the ones before that works .

--- pages/results.py
+++ pages/results.py
@@ -409,7 +409,7 @@ if __name__ == "__main__":
                             "Skills": str(skills)
                         }
                         results = db.child("users").child(str(localId)).child("Jobs").push(data)
-                        switch_page("dashboard")
+                        # switch_page("dashboard")
 
 
             st.markdown("""
