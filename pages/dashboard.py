4ef6b7e3eea94805dae7100e148f81058293fbde
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-13 09:30:34 -0700

    the one before this and the ones before that works .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -168,7 +168,7 @@ if __name__ == "__main__":
                 color:black
                
                 }
-                #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 {
+                #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-k1vhr4.egzxvld5 > div.block-container.css-k1ih3n.egzxvld4 > div:nth-child(1) > div > div:nth-child(9){
                 margin-top:-100px;
                 }
                 </style>""", unsafe_allow_html=True)
