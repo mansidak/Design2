5901e262c23fef4c5e62121febdb41057b213af6
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-13 14:32:34 -0700

    the one before this and the ones before that works .

--- pages/results.py
+++ pages/results.py
@@ -325,7 +325,7 @@ if __name__ == "__main__":
                         with col2mark:
                             st.write("")
                             Save = st.empty()
-                            if Save.checkbox("", key = f"{link}+{title}+{shortSummary}+{companyName}", value=False):
+                            if Save.checkbox("", key = f"{link}+{title}+{shortSummary}+{companyName}"):
                                 data = {
                                     "Link": str(link),
                                     "Title": str(title),
