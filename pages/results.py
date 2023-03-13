d0ab5b929708804ec53eada4ec0910917e2d7b2b
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-12 20:45:07 -0700

    the one before this and the ones before that works .

--- pages/results.py
+++ pages/results.py
@@ -426,6 +426,7 @@ if __name__ == "__main__":
             }
             
             div[data-testid="stFormSubmitButton"]{
+            margin-top:-340px;
             position:fixed;
             }
 
