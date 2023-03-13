f338cded8e5d8175e1314a654ceaf9c2db17ea53
Mansidak Singh <mansidaknoor@gmail.com>
2023-03-13 11:04:16 -0700

    the one before this and the ones before that works .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -501,8 +501,9 @@ if __name__ == "__main__":
 
                                
                                .stApp {
-                                 background-image: url("https://static1.squarespace.com/static/63c3021120578c74f330b74c/t/640f64a5f5808a1768beadeb/1678730406394/Dashboard.png");
-                                 background-attachment: local;
+                                 background-image: url("https://static1.squarespace.com/static/63c3021120578c74f330b74c/t/640f6580fd145910b7916911/1678730624838/Dashboard.png");
+                                 background-attachment: fixed;
+                                 background-size: cover;
                                  color:black
                                 }
                                                    
